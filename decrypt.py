ec0ee5be00dd10316265e0aa720bbf47544980c4
Alexandre Martin <alexandre@m4rtin.xyz>
2018-03-13 10:37:23 +0100

    Customize function.

--- decrypt.py
+++ decrypt.py
@@ -1,28 +1,31 @@
 import numpy as np
+from sympy import Matrix
 import string
 
 # Define variables
 key = np.matrix([[11, 3], [4, 5]])
-message = 'HILLCRYPT'
+message = 'XQYVVPXPTC'
 
 # Generate the alphabet
 alphabet = string.ascii_uppercase
 
 # Encrypted message
-encryptedMessage = ""
+decryptedMessage = ""
+
+# Get the decrypt key
+key = Matrix(key)
+key = key.inv_mod(26)
+key = key.tolist()
 
 # Group message in vectors and generate crypted message
 for index, i in enumerate(message):
     if index % 2 == 0:
         firstValue = alphabet.index(i)
-        if index + 1 < len(message):
-            secondValue = alphabet.index(message[index + 1])
-        else:
-            secondValue = 6
+        secondValue = alphabet.index(message[index + 1])
         vector = np.matrix([[firstValue], [secondValue]])
         vector = key * vector
         vector %= 26
-        encryptedMessage += alphabet[vector.item(0)] + alphabet[vector.item(1)]
+        decryptedMessage += alphabet[int(vector.item(0))] + alphabet[int(vector.item(1))]
 
 # Show the result
-print(encryptedMessage)
+print(decryptedMessage)
